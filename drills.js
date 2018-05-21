b97cf73923e6cb0db6f897e9c5bd100e5e5d7a1a
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-21 09:17:20 -0700

    set and get

--- drills.js
+++ drills.js
@@ -80,3 +80,25 @@ class HashMap {
 
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
+
+const lor = new HashMap
+
+function main() {
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Wizard', 'Gandolf');
+  lor.set('Human', 'Aragon');
+  lor.set('Elf', 'Legolas');
+  lor.set('Maiar', 'The Necromancer');
+  lor.set('Maiar', 'Sauron');
+  lor.set('RingBearer', 'Gollum');
+  lor.set('LadyOfLight', 'Galadriel');
+  lor.set('HalfElven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
+  
+  console.log(lor.get('Maiar'))
+}
+
+// ===INVOCATIONS===
+
+main();
