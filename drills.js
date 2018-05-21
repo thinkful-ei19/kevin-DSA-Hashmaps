045749d735c3ca2337808f56445175deb38541f1
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-21 11:33:34 -0700

    palindrome

--- drills.js
+++ drills.js
@@ -1,85 +1,118 @@
 class HashMap {
-  constructor(initialCapacity=8) {
-      this.length = 0;
-      this._slots = [];
-      this._capacity = initialCapacity;
-      this._deleted = 0;
+  constructor(initialCapacity = 8) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
   }
 
   get(key) {
-      const index = this._findSlot(key);
-      if (this._slots[index] === undefined) {
-          throw new Error('Key error');
-      }
-      return this._slots[index].value;
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      return null;
+    }
+    return this._slots[index].value;
   }
 
   set(key, value) {
-      const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-      if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-          this._resize(this._capacity * HashMap.SIZE_RATIO);
-      }
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
 
-      const index = this._findSlot(key);
-      this._slots[index] = {
-          key,
-          value,
-          deleted: false
-      };
-      this.length++;
+    const index = this._findSlot(key);
+    this._slots[index] = {
+      key,
+      value,
+      deleted: false
+    };
+    this.length++;
   }
 
   remove(key) {
-      const index = this._findSlot(key);
-      const slot = this._slots[index];
-      if (slot === undefined) {
-          throw new Error('Key error');
-      }
-      slot.deleted = true;
-      this.length--;
-      this._deleted++;
+    const index = this._findSlot(key);
+    const slot = this._slots[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
+    }
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
   }
 
   _findSlot(key) {
-      const hash = HashMap._hashString(key);
-      const start = hash % this._capacity;
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
 
-      for (let i=start; i<start + this._capacity; i++) {
-          const index = i % this._capacity;
-          const slot = this._slots[index];
-          if (slot === undefined || (slot.key == key && !slot.deleted)) {
-              return index;
-          }
+    for (let i = start; i < start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      if (slot === undefined || (slot.key == key && !slot.deleted)) {
+        return index;
       }
+    }
   }
 
   _resize(size) {
-      const oldSlots = this._slots;
-      this._capacity = size;
-      // Reset the length - it will get rebuilt as you add the items back
-      this.length = 0;
-      this._deleted = 0;
-      this._slots = [];
+    const oldSlots = this._slots;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items back
+    this.length = 0;
+    this._deleted = 0;
+    this._slots = [];
 
-      for (const slot of oldSlots) {
-          if (slot !== undefined && !slot.deleted) {
-              this.set(slot.key, slot.value);
-          }
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.deleted) {
+        this.set(slot.key, slot.value);
       }
+    }
   }
 
   static _hashString(string) {
-      let hash = 5381;
-      for (let i=0; i<string.length; i++) {
-          hash = (hash << 5) + hash + string.charCodeAt(i);
-          hash = hash & hash;
-      }
-      return hash >>> 0;
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
   }
 }
 
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
+
+// ===IS STRING A PALINDROME?===
+
+function palindrome(string) {
+  const hashMap = new HashMap;
+  // const letters = string.split('');
+
+  for (let i = 0; i < string.length; i++) {
+    if (hashMap.get(string[i])) {
+      hashMap.set(string[i], hashMap.get(string[i]) + 1)
+    } else {
+      hashMap.set(string[i], 1)
+    }
+  }
+
+  let counter = 0;
+  for (let i = 0; i < hashMap.length; i++) {
+    if (hashMap.get(string[i]) % 2 === 0) {
+      continue
+    } else if (counter < 2) {
+      counter ++;
+      if (counter === 2) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+// ===LORD OF THE RINGS HASHMAP===
 
 const lor = new HashMap
 
@@ -95,10 +128,13 @@ function main() {
   lor.set('LadyOfLight', 'Galadriel');
   lor.set('HalfElven', 'Arwen');
   lor.set('Ent', 'Treebeard');
-  
-  console.log(lor.get('Maiar'))
+
+  // console.log(lor);
+  // console.log(lor.get('Maiar'))
 }
 
 // ===INVOCATIONS===
 
 main();
+console.log(palindrome('acecarr'));
+
